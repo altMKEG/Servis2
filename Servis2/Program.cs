9486fb1f8b93c5d4719a56c9d612ef1688516ef6
Mehmet Kerim Gündemir <gundemirkerim9@gmail.com>
2024-11-26 15:06:31 +0300

    Proje dosyası ekle.

---
+++ Servis2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servis2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ogrenciListe = new List<string>();
+           
+
+            int ogrencisayi = 0;
+
+            Console.WriteLine(" geziye gelenler");
+            while (true)
+            {
+
+                int karar = 0;
+                karar = Convert.ToInt32(Console.ReadLine());
+
+                if (karar == 1)
+                {
+                    ogrenciListe.Add(Console.ReadLine());
+                    ogrencisayi++;
+                }
+                else if (karar == 2)
+                {
+                    Console.WriteLine(" top eklenen" + ogrencisayi);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("geçersiz");
+
+                }
+
+                Console.WriteLine("araç kaç kişşi");
+                int kapasite = Convert.ToInt32(Console.ReadLine());
+                if (ogrencisayi <= kapasite)
+                {
+                }
+            }
+        }
+    }
+}
